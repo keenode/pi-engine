10aaf96442fbcd0bc1823724591db78ef5e4cb32
keenode <kstaffieri@gmail.com>
2018-11-17 19:12:05 -0800

    Feat: Add router and layouts

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,19 @@
 import React from "react";
-import logo from "./logo.svg";
+import { Route, Switch } from "react-router-dom";
+
 import "./App.css";
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit
-        <code>src/App.js</code>
-        and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React. Please!
-      </a>
-    </header>
-  </div>
-);
+import MainLayout from "./layouts/MainLayout/MainLayout";
+import Home from "./containers/Home/Home";
+
+const App = () => {
+  const routes = (
+    <Switch>
+      <Route path="/" exact component={Home} />
+    </Switch>
+  );
+
+  return <MainLayout>{routes}</MainLayout>;
+};
 
 export default App;
