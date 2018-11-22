6f1e3d286bb73c919cb5b3d35b9307aa9ad1fe45
keenode <kstaffieri@gmail.com>
2018-11-21 23:39:46 -0800

    Add first person controls

--- src/components/WorldRenderer/WorldRenderer.jsx
+++ src/components/WorldRenderer/WorldRenderer.jsx
@@ -4,6 +4,8 @@ import Stats from "stats-js";
 
 import styles from "./WorldRenderer.module.scss";
 
+import FirstPersonControls from "first-person-controls";
+
 const CANVAS_WIDTH = window.innerWidth;
 const CANVAS_HEIGHT = window.innerHeight;
 const VIEW_ANGLE = 75;
@@ -31,14 +33,31 @@ class WorldRenderer extends Component {
     this.stats.domElement.style.top = "0px";
     document.body.appendChild(this.stats.domElement);
 
+    this.controls = new FirstPersonControls(this.camera);
+    this.controls.lookSpeed = 0.25;
+    this.controls.movementSpeed = 10;
+
+    this.clock = new THREE.Clock(true);
+
     this.scene.add(this.camera);
 
-    this.camera.position.z = 5;
+    // this.camera.position.z = 10;
 
+    // Cube
     const geometry = new THREE.BoxGeometry(1, 1, 1);
     const material = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
     this.cube = new THREE.Mesh(geometry, material);
     this.scene.add(this.cube);
+
+    // Buffer Plane
+    const geometry2 = new THREE.PlaneBufferGeometry(1, 1, 1);
+    const material2 = new THREE.MeshBasicMaterial({
+      color: 0xffff00,
+      side: THREE.DoubleSide
+    });
+    const plane = new THREE.Mesh(geometry2, material2);
+    plane.position.z = 2;
+    this.scene.add(plane);
 
     const pointLight = new THREE.PointLight(0xffffff);
     pointLight.position.x = 10;
@@ -56,6 +75,8 @@ class WorldRenderer extends Component {
 
     this.cube.rotation.x += 0.01;
     this.cube.rotation.y += 0.01;
+
+    this.controls.update(this.clock.getDelta());
 
     this.renderer.render(this.scene, this.camera);
     this.stats.end();
