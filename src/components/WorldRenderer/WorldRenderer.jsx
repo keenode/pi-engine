f10697d643afddfcb8ebbc5da9ee0f9e255766c0
keenode <kstaffieri@gmail.com>
2018-11-26 23:56:05 -0800

    Work on wall meshes

--- src/components/WorldRenderer/WorldRenderer.jsx
+++ src/components/WorldRenderer/WorldRenderer.jsx
@@ -14,10 +14,14 @@ const NEAR = 0.1;
 const FAR = 1000;
 
 const player = {
-  height: 1.0,
+  height: 2.0,
   speed: 0.2,
   turnSpeed: Math.PI * 0.01
 };
+
+const tileSize = 4.0;
+const mapFloor = [[1, 1, 1, 1], [0, 0, 0, 1], [0, 0, 0, 1]];
+const mapWall = [[0, 0, 0, 0], [1, 1, 1, 0], [0, 0, 1, 0]];
 
 class WorldRenderer extends Component {
   componentDidMount() {
@@ -29,6 +33,8 @@ class WorldRenderer extends Component {
     });
     this.renderer.setSize(CANVAS_WIDTH, CANVAS_HEIGHT);
     this.renderer.setClearColor(0x2a5263, 1.0);
+    this.renderer.shadowMap.enabled = true;
+    this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
     this.camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
     this.scene = new THREE.Scene();
 
@@ -50,34 +56,77 @@ class WorldRenderer extends Component {
     this.camera.position.set(0, player.height, -5);
     this.camera.lookAt(new THREE.Vector3(0, player.height, 0));
 
-    // Floor
-    const meshFloor = new THREE.Mesh(
-      new THREE.PlaneGeometry(10, 10, 10, 10),
-      new THREE.MeshLambertMaterial({ color: 0xffffff, wireframe: false })
-    );
-    meshFloor.rotation.x = -Math.PI / 2;
-    this.scene.add(meshFloor);
-
-    // Cube
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
-    this.cube = new THREE.Mesh(geometry, material);
-    this.cube.position.y = 1;
-    this.scene.add(this.cube);
-
-    // Point Light
-    const pointLight = new THREE.PointLight(0xffffff);
-    pointLight.position.set(0, 5, 0);
-    this.scene.add(pointLight);
-
-    this.renderer.render(this.scene, this.camera);
-    requestAnimationFrame(this.renderLoop);
-
     // Bind key events
     this.keyboard = [];
     window.addEventListener("keydown", this.keyDown);
     window.addEventListener("keyup", this.keyUp);
+
+    this.generateMap();
+
+    this.renderer.render(this.scene, this.camera);
+    requestAnimationFrame(this.renderLoop);
   }
+
+  generateMap = () => {
+    for (let x = 0; x < mapFloor.length; x += 1) {
+      for (let y = 0; y < mapFloor[0].length; y += 1) {
+        if (mapFloor[x][y] === 1) {
+          // Floor
+          const meshFloor = new THREE.Mesh(
+            new THREE.PlaneBufferGeometry(tileSize, tileSize, 1, 1),
+            new THREE.MeshLambertMaterial({ color: 0x00ffff, wireframe: false })
+          );
+          meshFloor.position.set(x * tileSize, 0, y * tileSize);
+          meshFloor.rotation.x = -Math.PI / 2;
+          meshFloor.receiveShadow = true;
+          this.scene.add(meshFloor);
+        }
+      }
+    }
+
+    for (let x = 0; x < mapWall.length; x += 1) {
+      for (let y = 0; y < mapWall[0].length; y += 1) {
+        if (mapWall[x][y] === 1) {
+          // Wall
+          const meshWall = new THREE.Mesh(
+            new THREE.BoxBufferGeometry(tileSize, tileSize, tileSize),
+            new THREE.MeshLambertMaterial({ color: 0xffffff, wireframe: false })
+          );
+          meshWall.position.set(x * tileSize, tileSize / 2, y * tileSize);
+          meshWall.rotation.x = -Math.PI / 2;
+          meshWall.receiveShadow = true;
+          const faces = meshWall.geometry;
+          console.log(faces);
+          this.scene.add(meshWall);
+        }
+      }
+    }
+
+    // Cube
+    const geometry = new THREE.BoxBufferGeometry(1, 1, 1);
+    const material = new THREE.MeshLambertMaterial({
+      color: 0x00ff00,
+      wireframe: false
+    });
+    this.cube = new THREE.Mesh(geometry, material);
+    this.cube.position.y = 1.5;
+    this.cube.castShadow = true;
+    this.scene.add(this.cube);
+
+    // Point Light
+    const pointLight = new THREE.PointLight(0xffffff, 1, 100);
+    pointLight.position.set(0, 5, 0);
+    pointLight.castShadow = true;
+    pointLight.shadow.mapSize.width = 2048;
+    pointLight.shadow.mapSize.height = 2048;
+    pointLight.shadow.camera.near = 0.5;
+    pointLight.shadow.camera.far = 500;
+    this.scene.add(pointLight);
+
+    // Ambient Light
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.25);
+    this.scene.add(ambientLight);
+  };
 
   keyDown = event => {
     this.keyboard[event.keyCode] = true;
