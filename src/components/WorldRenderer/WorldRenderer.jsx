23bb0d4b34325d7d6e123ebd242b9884075de34b
Keenan Staffieri <kstaffieri@fiveanddone.com>
2018-11-26 22:24:48 -0800

    Update cube position and mesh floor detail

--- src/components/WorldRenderer/WorldRenderer.jsx
+++ src/components/WorldRenderer/WorldRenderer.jsx
@@ -8,7 +8,7 @@ import styles from "./WorldRenderer.module.scss";
 
 const CANVAS_WIDTH = window.innerWidth;
 const CANVAS_HEIGHT = window.innerHeight;
-const VIEW_ANGLE = 75;
+const VIEW_ANGLE = 90;
 const ASPECT = CANVAS_WIDTH / CANVAS_HEIGHT;
 const NEAR = 0.1;
 const FAR = 1000;
@@ -52,8 +52,8 @@ class WorldRenderer extends Component {
 
     // Floor
     const meshFloor = new THREE.Mesh(
-      new THREE.PlaneGeometry(10, 10),
-      new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: false })
+      new THREE.PlaneGeometry(10, 10, 10, 10),
+      new THREE.MeshLambertMaterial({ color: 0xffffff, wireframe: false })
     );
     meshFloor.rotation.x = -Math.PI / 2;
     this.scene.add(meshFloor);
@@ -62,18 +62,10 @@ class WorldRenderer extends Component {
     const geometry = new THREE.BoxGeometry(1, 1, 1);
     const material = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
     this.cube = new THREE.Mesh(geometry, material);
+    this.cube.position.y = 1;
     this.scene.add(this.cube);
 
-    // Buffer Plane
-    const geometry2 = new THREE.PlaneBufferGeometry(1, 1, 1);
-    const material2 = new THREE.MeshBasicMaterial({
-      color: 0xffff00,
-      side: THREE.DoubleSide
-    });
-    const plane = new THREE.Mesh(geometry2, material2);
-    plane.position.z = 2;
-    this.scene.add(plane);
-
+    // Point Light
     const pointLight = new THREE.PointLight(0xffffff);
     pointLight.position.set(0, 5, 0);
     this.scene.add(pointLight);
@@ -88,15 +80,11 @@ class WorldRenderer extends Component {
   }
 
   keyDown = event => {
-    console.log("[keyDown]");
     this.keyboard[event.keyCode] = true;
-    console.log(this.keyboard);
   };
 
   keyUp = event => {
-    console.log("[keyUp]");
     this.keyboard[event.keyCode] = false;
-    console.log(this.keyboard);
   };
 
   renderLoop = () => {
