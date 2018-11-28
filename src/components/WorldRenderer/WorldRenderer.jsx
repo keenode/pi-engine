70b1488e6957a2b57ed5241e6455fd3ac06b948c
keenode <kstaffieri@gmail.com>
2018-11-28 00:31:10 -0800

    Transforming 3d map level properly now

--- src/components/WorldRenderer/WorldRenderer.jsx
+++ src/components/WorldRenderer/WorldRenderer.jsx
@@ -8,7 +8,7 @@ import styles from "./WorldRenderer.module.scss";
 
 const CANVAS_WIDTH = window.innerWidth;
 const CANVAS_HEIGHT = window.innerHeight;
-const VIEW_ANGLE = 90;
+const VIEW_ANGLE = 60;
 const ASPECT = CANVAS_WIDTH / CANVAS_HEIGHT;
 const NEAR = 0.1;
 const FAR = 1000;
@@ -20,8 +20,29 @@ const player = {
 };
 
 const tileSize = 4.0;
-const mapFloor = [[1, 1, 1, 1], [0, 0, 0, 1], [0, 0, 0, 1]];
-const mapWall = [[0, 0, 0, 0], [1, 1, 1, 0], [0, 0, 1, 0]];
+const mapMaxNegDepth = 1;
+const levelNeg1 = [
+  [0, 0, 0, 0, 0],
+  [1, 1, 1, 0, 0],
+  [0, 0, 1, 0, 0],
+  [0, 0, 1, 1, 0],
+  [0, 0, 1, 1, 0]
+];
+const level0 = [
+  [1, 1, 1, 0, 0],
+  [0, 0, 0, 1, 0],
+  [1, 1, 0, 1, 0],
+  [0, 1, 0, 0, 0],
+  [0, 1, 0, 0, 0]
+];
+const level1 = [
+  [0, 0, 0, 0, 0],
+  [1, 1, 1, 0, 0],
+  [0, 0, 1, 1, 0],
+  [0, 1, 0, 0, 0],
+  [0, 1, 0, 0, 0]
+];
+const mapGeometry = [levelNeg1, level0, level1];
 
 class WorldRenderer extends Component {
   componentDidMount() {
@@ -29,10 +50,10 @@ class WorldRenderer extends Component {
 
     this.renderer = new THREE.WebGLRenderer({
       canvas: $canvas,
-      antialias: false
+      antialias: true
     });
     this.renderer.setSize(CANVAS_WIDTH, CANVAS_HEIGHT);
-    this.renderer.setClearColor(0x2a5263, 1.0);
+    this.renderer.setClearColor(0x6cc9ff, 1.0);
     this.renderer.shadowMap.enabled = true;
     this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
     this.camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
@@ -53,8 +74,8 @@ class WorldRenderer extends Component {
 
     this.scene.add(this.camera);
 
-    this.camera.position.set(0, player.height, -5);
-    this.camera.lookAt(new THREE.Vector3(0, player.height, 0));
+    this.camera.position.set(0, player.height, 0);
+    this.camera.lookAt(new THREE.Vector3(0, player.height, 5));
 
     // Bind key events
     this.keyboard = [];
@@ -62,97 +83,98 @@ class WorldRenderer extends Component {
     window.addEventListener("keyup", this.keyUp);
 
     this.generateMap();
-    this.generateCustomCube();
 
     this.renderer.render(this.scene, this.camera);
     requestAnimationFrame(this.renderLoop);
   }
 
-  generateCustomCube = () => {
+  makeCube = (x, y, level) => {
     const cubeGroup = new THREE.Group();
 
     const sideFront = this.makeSideMesh(0xffff00);
+    sideFront.rotation.x = Math.PI;
     sideFront.position.set(0, tileSize / 2, -tileSize / 2);
-
-    const sideBottom = this.makeSideMesh(0xff0000);
-    sideBottom.rotation.x = Math.PI / 2;
-
-    const sideTop = this.makeSideMesh(0x00ff00);
-    sideTop.position.set(0, tileSize, 0);
-    sideTop.rotation.x = Math.PI / 2;
+    sideFront.receiveShadow = true;
 
     const sideBack = this.makeSideMesh(0x0000ff);
     sideBack.position.set(0, tileSize / 2, tileSize / 2);
+    sideBack.receiveShadow = true;
+
+    const sideTop = this.makeSideMesh(0x00ff00);
+    sideTop.position.set(0, tileSize, 0);
+    sideTop.rotation.x = -Math.PI / 2;
+    sideTop.receiveShadow = true;
+
+    const sideBottom = this.makeSideMesh(0xff0000);
+    sideBottom.rotation.x = Math.PI / 2;
+    sideBottom.receiveShadow = true;
+
+    const sideRight = this.makeSideMesh(0x00ffff);
+    sideRight.position.set(-tileSize / 2, tileSize / 2, 0);
+    sideRight.rotation.y = -Math.PI / 2;
+    sideRight.receiveShadow = true;
+
+    const sideLeft = this.makeSideMesh(0xff00ff);
+    sideLeft.position.set(tileSize / 2, tileSize / 2, 0);
+    sideLeft.rotation.y = Math.PI / 2;
+    sideLeft.receiveShadow = true;
 
     cubeGroup.add(sideFront);
-    cubeGroup.add(sideBottom);
-    cubeGroup.add(sideTop);
     cubeGroup.add(sideBack);
+    cubeGroup.add(sideTop);
+    cubeGroup.add(sideBottom);
+    cubeGroup.add(sideRight);
+    cubeGroup.add(sideLeft);
 
-    cubeGroup.position.set(-5, 0, 0);
-    this.scene.add(cubeGroup);
+    cubeGroup.position.set(x * tileSize, level * tileSize, y * tileSize);
+    return cubeGroup;
   };
 
   makeSideMesh = (color = 0xffffff) => {
     return new THREE.Mesh(
-      new THREE.PlaneBufferGeometry(tileSize, tileSize, 1, 1),
+      new THREE.PlaneBufferGeometry(tileSize, tileSize, 10, 10),
       new THREE.MeshLambertMaterial({
         color,
-        wireframe: false,
-        side: THREE.DoubleSide
+        wireframe: false
+        // side: THREE.DoubleSide
       })
     );
   };
 
   generateMap = () => {
-    for (let x = 0; x < mapFloor.length; x += 1) {
-      for (let y = 0; y < mapFloor[0].length; y += 1) {
-        if (mapFloor[x][y] === 1) {
-          // Floor
-          const meshFloor = new THREE.Mesh(
-            new THREE.PlaneBufferGeometry(tileSize, tileSize, 1, 1),
-            new THREE.MeshLambertMaterial({ color: 0x00ffff, wireframe: false })
-          );
-          meshFloor.position.set(x * tileSize, 0, y * tileSize);
-          meshFloor.rotation.x = -Math.PI / 2;
-          meshFloor.receiveShadow = true;
-          this.scene.add(meshFloor);
+    const map = new THREE.Group();
+    for (let level = 0; level < mapGeometry.length; level += 1) {
+      for (let x = 0; x < mapGeometry[level].length; x += 1) {
+        for (let y = 0; y < mapGeometry[level][0].length; y += 1) {
+          if (mapGeometry[level][x][y] === 1) {
+            const cube = this.makeCube(x, y, level - mapMaxNegDepth);
+            map.add(cube);
+          }
         }
       }
     }
-
-    for (let x = 0; x < mapWall.length; x += 1) {
-      for (let y = 0; y < mapWall[0].length; y += 1) {
-        if (mapWall[x][y] === 1) {
-          // Wall
-          const meshWall = new THREE.Mesh(
-            new THREE.BoxBufferGeometry(tileSize, tileSize, tileSize),
-            new THREE.MeshLambertMaterial({ color: 0xffffff, wireframe: false })
-          );
-          meshWall.position.set(x * tileSize, tileSize / 2, y * tileSize);
-          meshWall.rotation.x = -Math.PI / 2;
-          meshWall.receiveShadow = true;
-          const faces = meshWall.geometry;
-          console.log(faces);
-          this.scene.add(meshWall);
-        }
-      }
-    }
+    const mS = new THREE.Matrix4().identity();
+    mS.elements[0] = -1;
+    map.applyMatrix(mS);
+    map.rotation.y = -Math.PI / 2;
+    map.position.x = (mapGeometry[0][0].length * tileSize) / 2;
+    map.position.z = mapGeometry[0][0].length * tileSize - tileSize / 2;
+    this.scene.add(map);
 
     // Cube
-    const geometry = new THREE.BoxBufferGeometry(1, 1, 1);
+    const geometry = new THREE.BoxBufferGeometry(0.5, 0.5, 0.5);
     const material = new THREE.MeshLambertMaterial({
-      color: 0x00ff00,
+      color: 0xff0000,
       wireframe: false
     });
     this.cube = new THREE.Mesh(geometry, material);
-    this.cube.position.y = 1.5;
+    this.cube.position.set(0, 1.5, 5);
     this.cube.castShadow = true;
     this.scene.add(this.cube);
 
     // Point Light
     const pointLight = new THREE.PointLight(0xffffff, 1, 100);
-    pointLight.position.set(0, 5, 0);
+    pointLight.position.set(0, 5, 5);
     pointLight.castShadow = true;
     pointLight.shadow.mapSize.width = 2048;
     pointLight.shadow.mapSize.height = 2048;
